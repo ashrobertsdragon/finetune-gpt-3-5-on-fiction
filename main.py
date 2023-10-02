c8d6a0ee88b4df3c0b954cd7d44af31d4521c381
ashrobertsdragon <128648365+ashrobertsdragon@users.noreply.github.com>
2023-10-02 04:22:27 +0000

    Somehow a lot of things got messed up :shrug:

--- main.py
+++ main.py
@@ -4,141 +4,156 @@ import os
 import tiktoken
 from collections import deque
 import openai
+import time
 
 def read_text_file(file_path):
 
   try:
     with open(file_path, "r") as f:
-    read_file = f.read()
+      read_file = f.read()
 
 
-		return read_file
+    return read_file
 
-	except FileNotFoundError:
-		print(f"Error: File '{file_path}' not found.")
-		exit()
+  except FileNotFoundError:
+    print(f"Error: File '{file_path}' not found.")
+    exit()
 
 def write_jsonl_file(content, file_path):
   
-	with open(file_path, "a") as f:
-		for item in content:
-			json.dump(item, f)
-			f.write("\n")
+  with open(file_path, "a") as f:
+    for item in content:
+      json.dump(item, f)
+      f.write("\n")
 
 
-	return
+  return
 
 def separate_into_chapters(text):
 
 
-	return re.split("\s*\*\*\s*", text)
+  return re.split(r"\s*\*\*\s*", text)
 
 def split_into_chunks(chapter, chunk_size = 2000):
-	tokenizer = tiktoken.get_encoding("cl100k_base")
-	tokens = tokenizer.encode(chapter)
-	chunks = []
-	for i in range(0, len(tokens), chunk_size):
-		chunk_tokens = tokens[i:i + chunk_size]
-		chunks.append(tokenizer.decode(chunk_tokens))
+
+  tokenizer = tiktoken.get_encoding("cl100k_base")
+  tokens = tokenizer.encode(chapter)
+  chunks = []
+
+  for i in range(0, len(tokens), chunk_size):
+    chunk_tokens = tokens[i:i + chunk_size]
+    chunks.append(tokenizer.decode(chunk_tokens))
 
 
-	return chunks
+  return chunks
 
 def format_for_fine_tuning(chunks):
   
-	author = input("What is your author name? > ")
+  author = input("What is your author name? > ")
   system_message = f"You are a junior author who writes in the style of {author}"
   formatted_data = []
   queue = deque(chunks)
+
   while len(queue) > 1:
 
     user_message = queue.popleft()
     assistant_message = queue[0]
     message = {
-        "messages": [
-            { "role": "system", "content": system_message },
-            { "role": "user", "content": user_message },
-            { "role": "assistant", "content": assistant_message }
-        ]
+      "messages": [
+        { "role": "system", "content": system_message },
+        { "role": "user", "content": user_message },
+        { "role": "assistant", "content": assistant_message }
+      ]
     }
+
     formatted_data.append(message)
 
 
-    return formatted_data
-
-def process_files():
-  
-	books = "books"
-	fine_tune_messages = []
-  for file_name in os.path.dir(books):
-    if file_name == ".gitkeep":
-      continue
-    else:
-    	for root, _, files in os.walk(books):
-		    for file in files:
-    			file_path = os.path.join(root, file)
-		    	content = read_text_file(file_path)
-    			chapters = separate_into_chapters(content)
-
-		    	for chapter in chapters:
-				    chunks = split_into_chunks(chapter)
-    				formatted_messages = format_for_fine_tuning(chunks)
-		    		fine_tune_messages.extend(formatted_messages)
-    			print(f"{file} processed")
-
-	write_jsonl_file(fine_tune_messages, "fine_tune.jsonl")
-	print("All files processed")
+  return formatted_data
 
 def fine_tune(file_path):
   
-	openai.api_key = os.environ.get("OPENAI_API_KEY")
+  openai.api_key = os.environ.get("OPENAI_API_KEY")
 
-	JSONL_file = openai.File.create(file=open("fine_tune.jsonl", "rb"), purpose="fine-tune")
+  JSONL_file = openai.File.create(file=open("fine_tune.jsonl", "rb"), purpose="fine-tune")
 
-	print("Uploaded file id", JSONL_file.id)
+  print("Uploaded file id", JSONL_file.id)
 
-	while True:
+  while True:
 
-		upload_file = openai.File.retrieve(id=JSONL_file.id)
+    upload_file = openai.File.retrieve(id=JSONL_file.id)
 
-    os.sys("clear")
+    os.system("clear")
     print("Processing file.")
     time.sleep(1)
-    os.sys("clear")
+    os.system("clear")
     print("Processing file..")
     time.sleep(1)
-    os.sys("clear")
+    os.system("clear")
     print("Processing file...")
     time.sleep(1)
 
-		if upload_file.status == "processed":
-      os.sys("clear")
-			print("File processed")
-			break
+    if upload_file.status == "processed":
+      os.system("clear")
+      print("File processed")
+      break
 
-	fine_tune_job = openai.FineTuningJob.create(training_file=JSONL_file.id, model="gpt-3.5-turbo")
+  fine_tune_job = openai.FineTuningJob.create(training_file=JSONL_file.id, model="gpt-3.5-turbo")
 
-	while True:
+  while True:
 
-		fine_tune_info = openai.FineTuningJob.retrieve(id=fine_tune_job.id)
+    fine_tune_info = openai.FineTuningJob.retrieve(id=fine_tune_job.id)
 
-    os.sys("clear")
+    os.system("clear")
     print("Fine-tuning.")
     time.sleep(1)
-    os.sys("clear")
+    os.system("clear")
     print("Fine-tuning..")
     time.sleep(1)
-    os.sys("clear")
+    os.system("clear")
     print("Fine-tuning...")
     time.sleep(1)
 
-		if fine_tune_info.status == "succeeded":
-			print("fine_tune_info.status)
-			print("Fine-tuned model info", fine_tune_info)
-			print("Model id", fine_tune_info.fine_tuned_model)
+    if fine_tune_info.status == "succeeded":
+      print(fine_tune_info.status)
+      print("Fine-tuned model info", fine_tune_info)
+      print("Model id", fine_tune_info.fine_tuned_model)
 
-			break
+      break
 
+
+  return
+
+def process_files():
+  
+  books = "books"
+  fine_tune_messages = []
+  for file_name in os.listdir(books):
+    if file_name == ".gitkeep":
+      continue
+    else:
+      for root, _, files in os.walk(books):
+        for file in files:
+          file_path = os.path.join(root, file)
+          content = read_text_file(file_path)
+          chapters = separate_into_chapters(content)
+
+          for chapter in chapters:
+            chunks = split_into_chunks(chapter)
+            formatted_messages = format_for_fine_tuning(chunks)
+            fine_tune_messages.extend(formatted_messages)
+
+          print(f"{file} processed")
+
+  write_jsonl_file(fine_tune_messages, "fine_tune.jsonl")
+  print("All files processed")
+
+
+  return
+
+def main():
+  process_files()
+  fine_tune("fine_tune.jsonl")
 
 if __name__ == '__main__':
-	main()
+  main()
